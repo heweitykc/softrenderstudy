07a616616623fe6f49f91371c130712e3ec30b76
callee <heweitykc@163.com>
2014-01-17 17:57:40 +0800

    dae模版上传

--- src/orgeconverter/main.cpp
+++ src/orgeconverter/main.cpp
@@ -3,7 +3,15 @@
 using namespace tinyxml2;
 
 XMLDocument doc;
+XMLDocument out;
 int main(){
-	doc.LoadFile("2.dae");
+	doc.LoadFile("tree05.xml");
+	out.LoadFile("template.dae");
+	XMLElement *scene=doc.RootElement();
+	XMLElement *surface=scene->FirstChildElement("submeshes")->FirstChildElement("submesh");
+	while (surface){
+		printf("%s\n", surface->FirstAttribute()->Name());
+		surface=surface->NextSiblingElement();
+	}
 	return 0;
 }
